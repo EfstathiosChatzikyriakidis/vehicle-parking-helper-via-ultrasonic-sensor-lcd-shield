1999c1e606a1732c2af95311d63ab65c5173d04e
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:40:53 +0300

    Change.

--- development/vehicle_parking_helper_via_ultrasonic_sensor_lcd_shield/vehicle_parking_helper_via_ultrasonic_sensor_lcd_shield.ino
+++ development/vehicle_parking_helper_via_ultrasonic_sensor_lcd_shield/vehicle_parking_helper_via_ultrasonic_sensor_lcd_shield.ino
@@ -1,7 +1,7 @@
 /*
  *  Vehicle Parking Helper Via Ultrasonic Sensor & LCD Keypad Shield.
  *
- *  Copyright (C) 2010 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
